26ccf2b4cf7b91b90b3cfb241dbd294f69331c18
sebastian vasquez <seba.vasquez.m@gmail.com>
2020-02-20 22:52:25 -0300

    Add GetTracksByCollection Use case

---
+++ app/src/main/java/cl/svasquezm/glitunesplayer/data/usecases/GetTracksByCollectionUseCase.kt
@@ -0,0 +1,19 @@
+package cl.svasquezm.glitunesplayer.data.usecases
+
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import cl.svasquezm.glitunesplayer.domain.models.TrackDomainModel
+import cl.svasquezm.glitunesplayer.domain.repositories.TrackRepository
+import cl.svasquezm.glitunesplayer.utils.Constants
+
+class GetTracksByCollectionUseCase(private val repo: TrackRepository){
+    suspend fun execute(collectionId: String): LiveData<PagedList<TrackDomainModel>> {
+        val config = PagedList.Config.Builder()
+            .setInitialLoadSizeHint(Constants.PAGE_SIZE)
+            .setPageSize(Constants.PAGE_SIZE)
+            .build()
+
+        return LivePagedListBuilder<Int, TrackDomainModel>(repo.findAllTracksByCollection(collectionId), config).build()
+    }
+}
--- app/src/test/java/cl/svasquezm/glitunesplayer/tracklist/data/usecases/GetTracksByCollectionUseCaseTest.kt
+++ app/src/test/java/cl/svasquezm/glitunesplayer/tracklist/data/usecases/GetTracksByCollectionUseCaseTest.kt
@@ -1,6 +1,7 @@
 package cl.svasquezm.glitunesplayer.tracklist.data.usecases
 
 import androidx.paging.DataSource
+import cl.svasquezm.glitunesplayer.data.usecases.GetTracksByCollectionUseCase
 import cl.svasquezm.glitunesplayer.data.usecases.GetTracksByTermUseCase
 import cl.svasquezm.glitunesplayer.domain.models.TrackDomainModel
 import cl.svasquezm.glitunesplayer.domain.repositories.TrackRepository
@@ -10,22 +11,22 @@ import io.mockk.*
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 
-class GetTracksByTermUseCaseTest {
+class GetTracksByCollectionUseCaseTest {
 
     @Test
-    fun `Use case should return 1 tracks`() = runBlockingTest {
-        val term = "Foo"
+    fun `Use case should return 2 tracks`() = runBlockingTest {
+        val collectionId = "Foo.Bar"
         val factory = mockk<DataSource.Factory<Int, TrackDomainModel>>()
-        val list = listOf(mockk<TrackDomainModel>()).asPagedListLiveData()
+        val list = listOf(mockk(), mockk<TrackDomainModel>()).asPagedListLiveData()
 
         val repo = mockk<TrackRepository>()
-        val uc = GetTracksByTermUseCase(repo)
+        val uc: GetTracksByCollectionUseCase = GetTracksByCollectionUseCase(repo)
 
-        coEvery { repo.findAllTracks(term) } returns factory
-        coEvery { uc.execute(term) } returns list
+        coEvery { repo.findAllTracksByCollection(collectionId) } returns factory
+        coEvery { uc.execute(collectionId) } returns list
 
-        uc.execute(term)
+        uc.execute(collectionId)
 
-        coVerify { repo.findAllTracks(term) }
+        coVerify { repo.findAllTracksByCollection(collectionId) }
     }
 }
